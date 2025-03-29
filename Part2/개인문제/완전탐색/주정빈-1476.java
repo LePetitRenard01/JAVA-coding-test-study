a6a2bebfc3931aa78722cd0a16099e648e2f2bdf
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-03-30 01:52:34 +0900

    [Baekjoon-1476] 날짜 계산

---
+++ Part2/개인문제/완전탐색/주정빈-1476.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1476 {
+    static int E, S, M, result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        E = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = 1;
+
+        while ((result - E) % 15 != 0 || (result - S) % 28 != 0 || (result - M) % 19 != 0) {
+            result++;
+        }
+        System.out.println(result);
+    }
+}
