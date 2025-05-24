6a7b8c979ebd72ded9e140f569b11ccdf271186a
nimuyman <nimuyman@gmail.com>
2025-05-24 23:14:17 +0900

    [Baekjoon-1699] 제곱수의 합

---
+++ Part2/공통문제/dp/1699-남유민.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // 1로 만들 수 있는 경우
+
+            // j = 1; j * j <= i
+            // 모든 가능한 제곱수를 확인하면서 최소항 확정
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+
+        bw.write(dp[n] + "\n");
+        bw.flush();
+    }
+}
