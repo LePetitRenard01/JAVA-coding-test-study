87e7e46e5af9ef55b4da0f850806436dee6c42e6
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-07-22 01:01:46 +0900

    [Baekjoon-1789] 수들의 합

---
+++ Part3/개인문제/그리디/주정빈-1789.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class BOJ_1789 {
+    static long S, N, sum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        S = Long.parseLong(br.readLine());
+        N = 1;
+
+        while (true) {
+            if (sum + N > S) break;
+            else {
+                sum += N;
+                N++;
+            }
+        }
+        sb.append(N - 1);
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
